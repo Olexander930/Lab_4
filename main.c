e20fc7c8de51ec829f17d6b7bbf40858a06739f0
Іванченко Олександр <153652515+Olexander930@users.noreply.github.com>
2025-06-13 13:01:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,131 @@
 #include <stdio.h>
-#include <math.h>
 
-int main() {
+char field[3][3];     // Поле гри
+char activePlayer;    // Поточний гравець: 'X' або 'O'
 
-    int price = 450;
-    float znishka = 0.15;
-    int final_price = (int)(price * (1 - znishka));
-    printf("Ціна зі знижкою: %d грн\n", final_price);
+// Створення початкового порожнього поля
+void setupField()
+{
+    for (int row = 0; row < 3; ++row)
+        for (int col = 0; col < 3; ++col)
+            field[row][col] = ' ';
+}
 
-    int birth_year, current_year = 2025, age;
+// Виведення поля на консоль
+void displayField()
+{
+    printf("\n    1   2   3\n");
+    for (int i = 0; i < 3; ++i)
+    {
+        printf("  +---+---+---+\n");
+        printf("%d |", i + 1);
+        for (int j = 0; j < 3; ++j)
+        {
+            printf(" %c |", field[i][j]);
+        }
+        printf("\n");
+    }
+    printf("  +---+---+---+\n\n");
+}
 
-    printf("Введіть рік народження: ");
-    scanf("%d", &birth_year);
+// Перевірка на виграш
+char detectWinner()
+{
+    // Горизонталі та вертикалі
+    for (int i = 0; i < 3; ++i)
+    {
+        if (field[i][0] == field[i][1] && field[i][1] == field[i][2] && field[i][0] != ' ')
+            return field[i][0];
+        if (field[0][i] == field[1][i] && field[1][i] == field[2][i] && field[0][i] != ' ')
+            return field[0][i];
+    }
 
-    age = current_year - birth_year;
+    // Діагоналі
+    if (field[0][0] == field[1][1] && field[1][1] == field[2][2] && field[0][0] != ' ')
+        return field[0][0];
 
-    printf("Ваш вік: %d років\n", age);
+    if (field[0][2] == field[1][1] && field[1][1] == field[2][0] && field[0][2] != ' ')
+        return field[0][2];
 
+    return ' '; // Ніхто не виграв
+}
 
-    float x1, y1, x2, y2;
-    printf("Введіть координати першої точки (x y): ");
-    scanf("%f %f", &x1, &y1);
-    printf("Введіть координати другої точки (x y): ");
-    scanf("%f %f", &x2, &y2);
-    float distance = sqrt(pow((x2 - x1),2) + pow((y2 - y1),2));
-    printf("Відстань між точками: %.2f\n", distance);
+// Перевірка на нічию
+int isTie()
+{
+    for (int r = 0; r < 3; ++r)
+        for (int c = 0; c < 3; ++c)
+            if (field[r][c] == ' ')
+                return 0;
+    return 1;
+}
 
-    float prise, discount, discount_amount, final_prise;
+// Обробка ходу гравця
+void makeTurn()
+{
+    int r, c;
+    while (1)
+    {
+        printf("Гравець %c, введіть координати (рядок та стовпець через пробіл): ", activePlayer);
+        if (scanf("%d %d", &r, &c) != 2)
+        {
+            printf("Помилка вводу. Спробуйте знову.\n");
+            while (getchar() != '\n'); // очищення вводу
+            continue;
+        }
 
-    printf("Введіть вартість товару: ");
-    scanf("%f", &prise);
+        if (r < 1 || r > 3 || c < 1 || c > 3)
+        {
+            printf("Координати мають бути в межах 1-3.\n");
+            continue;
+        }
 
-    printf("Введіть відсоток знижки: ");
-    scanf("%f", &discount);
+        if (field[r - 1][c - 1] != ' ')
+        {
+            printf("Це місце вже зайняте. Оберіть інше.\n");
+            continue;
+        }
 
-    discount_amount = (prise * discount) / 100;
-    final_prise = prise - discount_amount;
+        field[r - 1][c - 1] = activePlayer;
+        break;
+    }
+}
 
-    printf("Кінцева вартість товару: %.f грн\n", final_prise);
+// Зміна гравця
+void togglePlayer()
+{
+    activePlayer = (activePlayer == 'X') ? 'O' : 'X';
+}
 
-    int number;
-    printf("Введіть тризначне число: ");
-    scanf("%d", &number);
-    int sum = number / 100 + (number / 10) % 10 + number % 10;
-    printf("Сума цифр: %d\n", sum);
+// Основна функція програми
+int main()
+{
+    activePlayer = 'X';
+    setupField();
 
+    printf("Починаємо гру \"Хрестики проти Нуликів\"!\n");
+
+    while (1)
+    {
+        displayField();
+        makeTurn();
+
+        char result = detectWinner();
+        if (result != ' ')
+        {
+            displayField();
+            printf("Гравець %c переміг! Вітаємо!\n", result);
+            break;
+        }
+
+        if (isTie())
+        {
+            displayField();
+            printf("Нічия. Дякуємо за гру!\n");
+            break;
+        }
+
+        togglePlayer();
+    }
     return 0;
 }
